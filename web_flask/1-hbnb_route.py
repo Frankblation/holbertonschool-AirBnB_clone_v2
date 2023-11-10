60a013c6caffc5a6e60c43ca9bf6b9f9de5bd1fa
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-10 01:35:17 -0600

    documented?

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""Starting Flask returning Hello HBNB!"""
+"""
+Simple Flask application returning Hello HBNB!
+"""
 
 from flask import Flask
 
-
+# Create a Flask web application instance
 app = Flask(__name__)
 
-
+# Define a route for the root URL ('/')
 @app.route('/', strict_slashes=False)
 def hi_there():
-    """Hello HBNB!"""
+    """
+    Route function for the root URL.
+    Returns: 'Hello HBNB!'
+    """
     return 'Hello HBNB!'
 
+# Define a route for '/hbnb'
 @app.route('/hbnb', strict_slashes=False)
-def hi_there():
-    """HBNB!"""
+def hi_hbnb():
+    """
+    Route function for the '/hbnb' URL.
+    Returns: 'HBNB'
+    """
     return 'HBNB'
 
+# Run the application if the script is executed directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
