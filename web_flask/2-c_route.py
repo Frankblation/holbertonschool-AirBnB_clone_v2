35bb2e3d41d0ebbddc9715cdc4dda6cd29bad58a
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-11-10 01:58:14 -0600

    2 display c

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Simple Flask application returning Hello HBNB!
+"""
+
+from flask import Flask
+
+# Create a Flask web application instance
+app = Flask(__name__)
+
+# Define a route for the root URL ('/')
+
+
+@app.route('/', strict_slashes=False)
+def hi_there():
+    """
+    Route function for the root URL.
+    Returns: 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+# Define a route for '/hbnb'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hi_hbnb():
+    """
+    Route function for the '/hbnb' URL.
+    Returns: 'HBNB'
+    """
+    return 'HBNB'
+
+# Define a route for /c/<tex>
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_c_text():
+    """
+    Route function for the '/c/<text>' URL.
+    Returns: 'C'
+    """
+    return 'C {}'.format(escape(text.replace('_', ' ')))
+
+# Run the application if the script is executed directly
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
